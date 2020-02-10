4e16ce4a07804038b6c63801795bc9d444d1743a
Huan Nguyen <huancal1984@gmail.com>
2020-02-09 19:36:16 -0800

    axios completed

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
-const mongoose = require("mongoose")
-const db = require("./models")
-console.log(db)
-mongoose.connect("mongodb://localhost/scrapedb", {useNewUrlParser:true, useUnifiedTopology: true})//.then(d=>console.log(d))
+const mongoose = require("mongoose");
+const colors = require("colors");
+const db = require("./models");
 
-axios.get("https://voices.berkeley.edu/").then(urlResponse => {
+
+console.log(db)
+mongoose.connect("mongodb://localhost/scrapedb", {useNewUrlParser:true, useUnifiedTopology: true})
+
+// axios.get("https://www.motor1.com/").then(urlResponse => {
+//     const $ = cheerio.load(urlResponse.data);
+//     $("div.views-field-title").each((i, element) => {
+//         let href= $(element).children().children().attr("href")
+//         let title= $(element).children().children().text()
+//         let author= $(element).siblings(".views-field-nothing").find("a").text()
+//         //console.log(counter, "--------", $(element).children().children().text())//grab image
+//         db.Article.create({ link: href, title: title, byline: author }).then(data=>console.log(data))
+//     })
+// });
+
+axios.get("https://www.huffpost.com/").then(urlResponse => {
     const $ = cheerio.load(urlResponse.data);
-       let counter=0
-    $("div.views-field-title").each((i, element) => {
-        let href= $(element).children().children().attr("href")
-        let title= $(element).children().children().text()
-        let author= $(element).siblings(".views-field-nothing").find("a").text()
-        //console.log(counter, "--------", $(element).children().children().text())//grab image
-        db.Article.create({ link: href, title: title, byline: author }).then(data=>console.log(data))
-        counter ++
+    
+
+    $("div.card__headlines").each((i, element) => {
+        const link = $(element).find("a").attr("href");
+
+        const header = $(element).find("a").text();
+
+        console.log(header.yellow);
+        console.log(link.cyan);
+        console.log("----------------------\n");
     })
+
+    $("picture").each((i, element) => {
+        const image = $(element).find("source").attr("srcset")
+        console.log(image);
+        console.log("----------------------\n");
+
+        
+    })
+    
+    
 });
+
